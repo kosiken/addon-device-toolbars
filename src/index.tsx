2c682daf1cd5476ff24189574aa91bbe2d72c2e0
Allison Kosy <Allisonkosy@gmail.com>
2025-02-11 09:41:30 +0100

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import { addons, types } from '@storybook/core/manager-api';
 
-import ControlsPanel from './ControlsPanel';
-import { AddonPanel } from './Panel';
+import { Text, View } from 'react-native';
 
 export const ADDON_ID = 'RNToolbar' as const;
 export const PARAM_KEY = 'toolbar' as const;
@@ -9,12 +8,16 @@ export const PARAM_KEY = 'toolbar' as const;
 export function register() {
   addons.register(ADDON_ID, (api) => {
     addons.add(ADDON_ID, {
-      type: types.PANEL,
-      title: 'Controls',
+      type: types.TOOL,
+      title: 'Toolbar',
       render: ({ active }) => (
-        <AddonPanel active={active}>
-          <ControlsPanel api={api} />
-        </AddonPanel>
+        <View style={{
+            backgroundColor: 'red',
+            width: '100%',
+            height: 50
+        }}>
+            <Text>Loading....</Text>
+        </View>
       ),
       paramKey: PARAM_KEY,
     });
